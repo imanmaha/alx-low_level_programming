9f89689a61bcdcb5f5d89bf1b9bd4d34e29aa348
imanmaha <imanemaha00@gmail.com>
2023-07-14 13:03:00 -0700

    hmama

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 /**
- * main Entry - point
- * Description: 'get the last digit of a number'
- * Return: always 0
+ * main - is n last digit positive, negative or zero
+ *
+ * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
