5bcfb145c652c4198b42a1dd85cfbaf88eac4f93
imanmaha <imanemaha00@gmail.com>
2023-07-14 12:38:37 -0700

    hmama

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ ld = n % 10;
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	if (ld == 0)
+	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	if (ld < 6 && ld != 0)
+	else if (ld < 6 && ld != 0)
 	{
 		printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
 	}
